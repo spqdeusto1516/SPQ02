f3536ab71e234189e323786a53992a7b6fd71479
Diana ARTIOM <diana.artiom24@gmail.com>
2017-03-30 17:09:31 +0200

    Created basic mvn project using eclipse. #B1702-26 Done

---
+++ bookit/src/main/java/com/deusto/bookit/App.java
@@ -0,0 +1,13 @@
+package com.deusto.bookit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Book a book!" );
+    }
+}
