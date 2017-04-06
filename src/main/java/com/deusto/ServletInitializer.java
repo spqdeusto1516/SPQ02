765c83ab755ecad2067ad91b2594e6af98b2dfa6
Diana ARTIOM <diana.artiom24@gmail.com>
2017-04-06 20:38:32 +0200

    Refactored ServletInitializer.

--- src/main/java/com/deusto/ServletInitializer.java
+++ src/main/java/com/deusto/ServletInitializer.java
@@ -1,13 +1,7 @@
 package com.deusto;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Spq02Application.class);
-	}
+public class ServletInitializer {
 
 }
