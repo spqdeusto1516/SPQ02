24b248b2f7c9a910a6764f6a935123ede8c6bd4d
ander <ander@192.168.1.44>
2017-04-19 13:08:35 +0200

    Archetype change

---
+++ Server/src/main/java/com/deusto/model/Address.java
@@ -0,0 +1,58 @@
+package com.deusto.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "addresses")
+public class Address {
+	@Id
+	private String id;
+	private String street;
+	private int houseNumber;
+	private int apartamentNumber;
+	private String postalCode;
+	private String city;
+	private String country;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public int getApartamentNumber() {
+		return apartamentNumber;
+	}
+	public void setApartamentNumber(int apartamentNumber) {
+		this.apartamentNumber = apartamentNumber;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
