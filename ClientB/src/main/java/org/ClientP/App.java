283886b8a526cf46f2b1435420c9f34ad7331e60
ander <ander.areizagab@opendeusto.es>
2017-05-04 13:51:09 +0200

    First try in connection #B1702-51

--- ClientB/src/main/java/org/ClientP/App.java
+++ ClientB/src/main/java/org/ClientP/App.java
@@ -2,9 +2,6 @@ package org.ClientP;
 
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -19,16 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.geo.GeoJson;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.test.web.servlet.MvcResult;
 
-import com.deusto.controllers.BookController;
-import com.deusto.integration.test.ReserveControllerITest;
-import com.deusto.models.Book;
-import com.deusto.repositories.BookRepository;
-import com.deusto.services.BookService;
+import com.deusto.model.Book;
 import com.mongodb.*;
 import com.mongodb.client.*;
 
@@ -36,43 +25,41 @@ public class App
 { 	
 	@SuppressWarnings( "resource" )
 	public static void main(String args[]) {
-		BookService bookit = new BookService();
-		System.out.println(bookit.findAll().get(0).getTitle());
-//		 MongoClient client = new MongoClient("localhost",27017); 
-//		 MongoDatabase db = client.getDatabase("bookit_db");
-//		 MongoCollection<Document> collection = db.getCollection("books");
-//
-//		 FindIterable<Document> iterable = collection.find();
-//		 MongoCursor<Document> cursor =  iterable.iterator();
-//		 ObjectMapper mapper = new ObjectMapper();
-//		 
-//		 String jsonInString;
-//		 int i = 0;
-//		 ArrayList<Book> books = new ArrayList<>();
-//		 while(cursor.hasNext()){
-//			 jsonInString = cursor.tryNext().toJson();
-//			 try {
-//				books.add(i,mapper.readValue(jsonInString, Book.class));
-//				i++;
-//			} catch (JsonParseException e) {
-//				e.printStackTrace();
-//			} catch (JsonMappingException e) {
-//				e.printStackTrace();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		 }
-//		 ArrayList<Book> bookByTitle = new ArrayList<>();
-//		 int j = 0;
-//		 for(Book book:books){
-//			 if(book.getTitle().equals("hola")){
-//				 bookByTitle.add(j, book);
-//				 j++;
-//			 }
-//		 }
-//		 for(Book book:bookByTitle){
-//			 System.out.println(book.getId());
-//		 }
+		 MongoClient client = new MongoClient("localhost",27017); 
+		 MongoDatabase db = client.getDatabase("bookit_db");
+		 MongoCollection<Document> collection = db.getCollection("books");
+
+		 FindIterable<Document> iterable = collection.find();
+		 MongoCursor<Document> cursor =  iterable.iterator();
+		 ObjectMapper mapper = new ObjectMapper();
+		 
+		 String jsonInString;
+		 int i = 0;
+		 ArrayList<Book> books = new ArrayList<>();
+		 while(cursor.hasNext()){
+			 jsonInString = cursor.tryNext().toJson();
+			 try {
+				books.add(i,mapper.readValue(jsonInString, Book.class));
+				i++;
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		 }
+		 ArrayList<Book> bookByTitle = new ArrayList<>();
+		 int j = 0;
+		 for(Book book:books){
+			 if(book.getTitle().equals("hola")){
+				 bookByTitle.add(j, book);
+				 j++;
+			 }
+		 }
+		 for(Book book:bookByTitle){
+			 System.out.println(book.getId());
+		 }
 	}
 
 }
--- ServerB/src/main/java/com/deusto/model/Book.java
+++ ServerB/src/main/java/com/deusto/model/Book.java
@@ -1,104 +1,117 @@
 package com.deusto.model;
 
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Document(collection="books")
-public class Book {
-  @Id
-  private String id;
-  private String title;
-  private String authorFirstName;
-  private String authorLastName;
-  private String genre;
-  private String description;
-  private Long publishDate;
-  private int pages;
-  private int ageLimit;
-  private int count;
-  private List<String> tags;
-  	public Book(){
-  	}
-  	public Book(String id, String title){
-  		this.id = id;
-  		this.title = title;
-  	}
-  	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthorFirstName() {
-		return authorFirstName;
-	}
-	
-	public void setAuthorFirstName(String authorFirstName) {
-		this.authorFirstName = authorFirstName;
-	}
+import com.deusto.repository.BookRepository;
 
-	public String getAuthorLastName() {
-		return authorLastName;
-	}
-	public void setAuthorLastName(String authorLastName) {
-		this.authorLastName = authorLastName;
-	}
-	public String getGenre() {
-		return genre;
-	}
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Long getPublishDate() {
-		return publishDate;
-	}
-	public void setPublishDate(Long publishDate) {
-		this.publishDate = publishDate;
-	}
-	public int getPages() {
-		return pages;
-	}
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-	public int getAgeLimit() {
-		return ageLimit;
-	}
-	public void setAgeLimit(int ageLimit) {
-		this.ageLimit = ageLimit;
-	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
-	public List<String> getTags() {
-		return tags;
-	}
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-	public String toString() {
-        return "Quote{" +
-                "ID='" + id + '\'' +
-                ", Title =" + title +
-                '}';
+import java.util.List;
+
+@JsonIgnoreProperties({"_id","publishDate","_class"})
+@Document(collection = "books")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String authorFirstName;
+    private String authorLastName;
+    private String genre;
+    private String description;
+    private Long publishDate;
+    private int pages;
+    private int ageLimit;
+    private int count;
+    private List<String> tags;
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Long publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+    
 }
