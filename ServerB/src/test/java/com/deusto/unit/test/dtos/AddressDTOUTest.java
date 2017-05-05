658759d6eed10887cfde83dacdd257275014959e
Diana ARTIOM <diana.artiom24@gmail.com>
2017-05-05 19:42:47 +0200

    B1702-54 - Started work on DTO testing.

--- ServerB/src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
+++ ServerB/src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
@@ -1,8 +1,48 @@
 package com.deusto.unit.test.dtos;
 
+import com.deusto.dtos.AddressDTO;
 import com.deusto.unit.test.common.AbstractUT;
-import org.junit.Ignore;
+import org.junit.Test;
 
-@Ignore
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class AddressDTOUTest extends AbstractUT {
+
+    @Test
+    public void validBookDTO() {
+        AddressDTO addressDTO = new AddressDTO();
+
+        addressDTO.setCountry("Moldova");
+        addressDTO.setStreet("Stephan the Great");
+        addressDTO.setCity("Chisinau");
+        addressDTO.setApartamentNumber(123);
+        addressDTO.setHouseNumber(666);
+        addressDTO.setPostalCode("4822");
+
+        assertTrue((validate(addressDTO).isEmpty()));
+
+        assertThat(addressDTO.getCity(), is("Chisinau"));
+        assertThat(addressDTO.getStreet(), is("Stephan the Great"));
+        assertThat(addressDTO.getCountry(), is("Moldova"));
+        assertThat(addressDTO.getApartamentNumber(), is(123));
+        assertThat(addressDTO.getHouseNumber(), is(666));
+        assertThat(addressDTO.getPostalCode(), is("4822"));
+    }
+
+    /* TODO - Review this test - the size() function */
+    @Test
+    public void invalidBookDTO() {
+        AddressDTO addressDTO = new AddressDTO();
+
+        addressDTO.setCountry("2");
+        addressDTO.setPostalCode("");
+
+        assertTrue(validate(addressDTO).isEmpty());
+        assertThat(validate(addressDTO).size(), is (1));
+    }
+
 }
