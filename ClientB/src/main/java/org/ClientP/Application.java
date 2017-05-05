93ffe6df54436aae998677579c9c7cdd5028ddc6
ander <ander.areizagab@opendeusto.es>
2017-05-05 15:42:24 +0200

    Connection avances #B1702-58 add work 1h Development

--- ClientB/src/main/java/org/ClientP/Application.java
+++ ClientB/src/main/java/org/ClientP/Application.java
@@ -2,42 +2,23 @@ package org.ClientP;
 
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.List;
+import com.deusto.models.Book;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.boot.json.*;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.hamcrest.Matchers.*;
-import org.json.JSONObject;
 import org.apache.http.*;
-import com.deusto.models.*;
 
-/**
- * This class is the same as the ApacheHttpRestClient2 class, but with
- * fewer try/catch clauses, and fewer comments.
-*/
 public class Application {
 	 
   public final static void main(String[] args)  {
 	
     HttpClient httpClient = HttpClientBuilder.create().build();
     try {
-      HttpGet httpGetRequest = new HttpGet("http://localhost:8080/");
+      HttpGet httpGetRequest = new HttpGet("http://localhost:8080/book");
       HttpResponse httpResponse = httpClient.execute(httpGetRequest);
 
       System.out.println("----------------------------------------");
@@ -48,23 +29,20 @@ public class Application {
       ArrayList<Book> books = new ArrayList<>();
       ObjectMapper mapper = new ObjectMapper();
 
-      byte[] buffer = new byte[500];
-      byte[] buffer2 = new byte[200];
-      byte[] buffer3 = new byte[200];
+      byte[] buffer = new byte[1024];
       if (entity != null) {
         InputStream inputStream = entity.getContent();
         try {
-          int bytesRead = 0;
           BufferedInputStream bis = new BufferedInputStream(inputStream);
           int i = 0;
           int j = 0;
           while((buffer[i] = (byte) bis.read()) != -1){
         	  if(buffer[i] == 125){
         		  String chunk = new String(buffer, 1, i);
+        		  System.out.println(chunk);
         		  books.add(j,mapper.readValue(chunk, Book.class));
         		  j++;
-        		  i = 0;
-        		  
+        		  i = 0;  
         	  }
         	  else{
         		  i++;
@@ -76,6 +54,8 @@ public class Application {
           System.out.println(books.get(3).getId());
           System.out.println(books.get(4).getId());
           System.out.println(books.get(5).getId());
+          System.out.println(books.get(6).getTitle());
+          System.out.println(books.get(6).getGenre());
         
         } catch (Exception e) {
           e.printStackTrace();
