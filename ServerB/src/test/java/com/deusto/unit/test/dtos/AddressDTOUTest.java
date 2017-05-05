763f66ef5086658f16e0522b8d78c3a68c0bf503
Diana ARTIOM <diana.artiom24@gmail.com>
2017-05-05 22:40:59 +0200

    B1702-54 DTO Unit Testing.

--- ServerB/src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
+++ ServerB/src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -32,17 +31,4 @@ public class AddressDTOUTest extends AbstractUT {
         assertThat(addressDTO.getHouseNumber(), is(666));
         assertThat(addressDTO.getPostalCode(), is("4822"));
     }
-
-    /* TODO - Review this test - the size() function */
-    @Test
-    public void invalidBookDTO() {
-        AddressDTO addressDTO = new AddressDTO();
-
-        addressDTO.setCountry("2");
-        addressDTO.setPostalCode("");
-
-        assertTrue(validate(addressDTO).isEmpty());
-        assertThat(validate(addressDTO).size(), is (1));
-    }
-
 }
--- ServerB/src/test/java/com/deusto/unit/test/dtos/PersonDTOUTest.java
+++ ServerB/src/test/java/com/deusto/unit/test/dtos/PersonDTOUTest.java
@@ -1,8 +1,70 @@
 package com.deusto.unit.test.dtos;
 
+import com.deusto.dtos.AddressDTO;
+import com.deusto.dtos.PersonDTO;
 import com.deusto.unit.test.common.AbstractUT;
-import org.junit.Ignore;
+import org.junit.Test;
 
-@Ignore
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class PersonDTOUTest extends AbstractUT {
+
+    @Test
+    public void validPersonDTO() {
+        PersonDTO personDTO = new PersonDTO();
+
+        personDTO.setBirthDate(0);
+
+        AddressDTO addressDTO = new AddressDTO();
+        addressDTO.setCountry("Moldova");
+        personDTO.setAddress(addressDTO);
+
+        personDTO.setEmail("email@example.com");
+        personDTO.setCountryCode("1234");
+        personDTO.setPassword("password");
+        personDTO.setPhone(123456789);
+
+        assertTrue(validate(personDTO).isEmpty());
+        assertThat(personDTO.getAddress().getCountry(), is("Moldova"));
+        assertThat(personDTO.getEmail(), is("email@example.com"));
+        assertThat(personDTO.getCountryCode(), is("1234"));
+        assertThat(personDTO.getPassword(), is("password"));
+        assertThat(personDTO.getPhone(), is(123456789));
+    }
+
+    @Test
+    public void invalidEmailPeronDTO() {
+
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setEmail("email.example.com");
+
+        assertFalse(validate(personDTO).isEmpty());
+    }
+
+    @Test
+    public void invalidEmptyEmailPersontDTO() {
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setEmail("");
+
+        assertFalse(validate(personDTO).isEmpty());
+    }
+
+    @Test
+    public void invalidEmptyCountryCodePersonDTO() {
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setCountryCode("");
+
+        assertFalse(validate(personDTO).isEmpty());
+    }
+
+    @Test
+    public void invalidEmptyPasswordPersonDTO() {
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setPassword("");
+
+        assertFalse(validate(personDTO).isEmpty());
+    }
 }
