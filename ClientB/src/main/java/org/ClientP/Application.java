2d01f0ac440f2b5ac7bd7ee9e8f3084873715fcc
ander <ander.areizagab@opendeusto.es>
2017-05-06 13:40:22 +0200

    RESFUL client start #B1702-58 add work 2h Development

--- ClientB/src/main/java/org/ClientP/Application.java
+++ ClientB/src/main/java/org/ClientP/Application.java
@@ -13,60 +13,74 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.apache.http.*;
 
 public class Application {
-	 
-  public final static void main(String[] args)  {
+		
 	
-    HttpClient httpClient = HttpClientBuilder.create().build();
-    try {
-      HttpGet httpGetRequest = new HttpGet("http://localhost:8080/book");
-      HttpResponse httpResponse = httpClient.execute(httpGetRequest);
-
-      System.out.println("----------------------------------------");
-      System.out.println(httpResponse.getStatusLine());
-      System.out.println("----------------------------------------");
-
-      HttpEntity entity = httpResponse.getEntity();
-      ArrayList<Book> books = new ArrayList<>();
-      ObjectMapper mapper = new ObjectMapper();
-
-      byte[] buffer = new byte[1024];
-      if (entity != null) {
-        InputStream inputStream = entity.getContent();
-        try {
-          BufferedInputStream bis = new BufferedInputStream(inputStream);
-          int i = 0;
-          int j = 0;
-          while((buffer[i] = (byte) bis.read()) != -1){
-        	  if(buffer[i] == 125){
-        		  String chunk = new String(buffer, 1, i);
-        		  System.out.println(chunk);
-        		  books.add(j,mapper.readValue(chunk, Book.class));
-        		  j++;
-        		  i = 0;  
-        	  }
-        	  else{
-        		  i++;
-        	  }
-          }
-          System.out.println(books.get(0).getId());
-          System.out.println(books.get(1).getId());
-          System.out.println(books.get(2).getId());
-          System.out.println(books.get(3).getId());
-          System.out.println(books.get(4).getId());
-          System.out.println(books.get(5).getId());
-          System.out.println(books.get(6).getTitle());
-          System.out.println(books.get(6).getGenre());
-        
-        } catch (Exception e) {
-          e.printStackTrace();
-        } finally {
-          try { inputStream.close(); } catch (Exception ignore) {}
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-    	httpClient.getConnectionManager().shutdown();
-    }
-  }
+	public HttpEntity startConnection(String url){
+		
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpGet httpGetRequest = new HttpGet(url);
+		HttpEntity entity = null;
+	    try {
+			HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+			entity = httpResponse.getEntity();
+			System.out.println("----------------------------------------");
+			System.out.println(httpResponse.getStatusLine());
+			System.out.println("----------------------------------------");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    return entity;
+	}
+	
+	public ArrayList<Book> getBooks(HttpEntity entity){
+		ArrayList<Book> books = new ArrayList<>();
+	    ObjectMapper mapper = new ObjectMapper();
+	    byte[] buffer = new byte[1024];
+	    if (entity != null) {
+	        InputStream inputStream = null;
+			try {
+				inputStream = entity.getContent();
+			} catch (UnsupportedOperationException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+	        try {
+	          BufferedInputStream bis = new BufferedInputStream(inputStream);
+	          int i = 0;
+	          int j = 0;
+	          while((buffer[i] = (byte) bis.read()) != -1){
+	        	  if(buffer[i] == 125){
+	        		  String chunk = new String(buffer, 1, i);
+	        		  System.out.println(chunk);
+	        		  books.add(j,mapper.readValue(chunk, Book.class));
+	        		  j++;
+	        		  i = 0;  
+	        	  }
+	        	  else{
+	        		  i++;
+	        	  }
+	          }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	       } finally {
+	          try { inputStream.close(); } catch (Exception ignore) {}
+	      }}
+		return books;
+	}
+	
+	public final static void main(String[] args)  {
+		Application app = new Application();
+		HttpEntity entity = app.startConnection("http://localhost:8080/book");
+		ArrayList<Book> books = app.getBooks(entity);
+		System.out.println(books.get(0).getId());
+		System.out.println(books.get(1).getId());
+		System.out.println(books.get(2).getId());
+		System.out.println(books.get(3).getId());
+		System.out.println(books.get(4).getId());
+		System.out.println(books.get(5).getId());
+		System.out.println(books.get(6).getTitle());
+		System.out.println(books.get(6).getGenre());  
+	}
  }
