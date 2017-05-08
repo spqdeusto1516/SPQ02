775789037c380c71c05dc15afe50416c5fa81856
Asier <asier.horas@opendeusto.es>
2017-05-08 10:19:14 +0200

    Client side model classes

---
+++ ClientB/src/main/java/org/models/Registr.java
@@ -0,0 +1,54 @@
+package org.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "registrs")
+public class Registr {
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private boolean isActiv;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActiv() {
+        return isActiv;
+    }
+
+    public void setActiv(boolean activ) {
+        isActiv = activ;
+    }
+}
